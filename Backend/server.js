cf5aa31171d443ce4ebe4f7dd80a453ad5336b36
Rabi22 <rabipradhan320@gmail.com>
2026-05-17 04:35:05 +0530

    c

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const connectDB = require("./src/config/database")
 
 connectDB()
 
-app.listen(3000,()=>{
-    console.log("Server running on Port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on Port ${PORT}`);
 })
